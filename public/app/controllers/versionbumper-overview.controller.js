9532adc6ba38d99d9c3f708cf295355deb38f2be
Tom Opdebeeck <tom230691@gmail.com>
2017-08-10 17:58:19 +0200

    updatet overview page

--- public/app/controllers/versionbumper-overview.controller.js
+++ public/app/controllers/versionbumper-overview.controller.js
@@ -5,25 +5,171 @@ angular
 	.controller("acpaasportalversionbumperOverviewController", [
 		"$scope",
 		"$timeout",
+		"$q",
 		"acpaasportalversionbumperFactory",
+		"acpaasportalversionbumperConfig",
 		"LabelService",
 		"NotificationService",
+		"DialogService",
 
 		function(
 			$scope,
 			$timeout,
+			$q,
 			acpaasportalversionbumperFactory,
+			acpaasportalversionbumperConfig,
 			LabelService,
-			NotificationService
+			NotificationService,
+			DialogService
 		) {
+			$scope.filters = {
+				type: "",
+				product: "",
+				api: "",
+				searchQuery: "",
+			};
+			$scope.types = [];
+			$scope.loading = {
+				types: false,
+				table: false,
+				search: false,
+			};
 			$scope.products = [];
+			$scope.apis = [];
+			$scope.tableData = [];
+			$scope.searchFieldOptions = {
+				debounce: 150,
+			};
+			$scope.pagination = {
+				currentPage: 0,
+				itemsPerPage: 10,
+				async: false,
+				itemsLength: 0,
+			};
+			$scope.tableConfig = {
+				pagination: $scope.pagination,
+				selects: [],
+				searchField: {
+					enabled: false,
+				},
+				columns: [{
+					columnName: LabelService.getString("Name"),
+					key: "meta.label",
+					sortable: true,
+				}, {
+					columnName: LabelService.getString("Description"),
+					key: "meta.description",
+					sortable: true,
+				}, {
+					columnName: LabelService.getString("Author"),
+					key: "meta.lastEditor.user"
+				}, {
+					columnName: LabelService.getString("Last edit"),
+					key: "meta.lastModified",
+					mode: "timeAgo",
+					defaultSort: true,
+					sortable: true,
+				}, {
+					columnName: LabelService.getString("Actions"),
+					template: "<a href=\"#\" ng-click=\"$emit('bumpVersion', i)\">" + LabelService.getString("Bump") + "</a>",
+				}],
+			};
 
 			function init() {
-				acpaasportalversionbumperFactory.fetchProducts()
-					.then(function(products) {
-						$scope.products = products;
+				// updateTable();
+				fetchTypes();
+			}
+
+			function fetchTypes() {
+				$scope.loading.types = true;
+				acpaasportalversionbumperFactory.fetchTypes()
+					.then(function(types) {
+						$scope.types = types.map(function(type) {
+							return {
+								label: type.type,
+								value: type.id,
+							};
+						});
+						$scope.loading.types = false;
 					});
 			}
+
+			function fetchType(type) {
+				switch (type.label) {
+					case "product":
+						return acpaasportalversionbumperFactory.fetchProducts();
+					case "api":
+						return acpaasportalversionbumperFactory.fetchApis();
+					default:
+						return $q.reject();
+				}
+			}
+
+			function fetchItemsForType(type) {
+				$scope.loading.table = true;
+
+				fetchType(type)
+					.then(function(items) {
+						$scope[type] = items;
+						$scope.tableData = filterItems(items, $scope.filters, type);
+						$scope.pagination.itemsLength = items.length;
+						$scope.loading.table = false;
+					}, function() {
+						$scope.loading.table = false;
+					});
+			}
+
+			function filterItems(items, options) {
+				return items.filter(function(item) {
+					if (!options.searchQuery) {
+						return true;
+					}
+
+					if (_.get(item, "meta.label", "").toLowerCase().indexOf(options.searchQuery.toLowerCase()) >= 0) {
+						return true;
+					}
+
+					if (_.get(item, "meta.description", "").toLowerCase().indexOf(options.searchQuery.toLowerCase()) >= 0) {
+						return true;
+					}
+
+					return false;
+				});
+			}
+
+			function searchItems(type) {
+				$scope.loading.search = true;
+				$scope.tableData = filterItems($scope[type], $scope.filters);
+
+				$timeout(function() {
+					$scope.loading.search = false;
+				}, 500);
+			}
+
+			function bumpVersion(event, item) {
+				var modalData = {
+					type: $scope.filters.type,
+					item: item,
+					versionInfo: {
+						label: _.get(item, "meta.label", ""),
+						major: 0,
+						minor: 0,
+						patch: 1,
+					},
+				};
+
+				DialogService.openModal({
+					templateUrl: acpaasportalversionbumperConfig.modulePath + "views/versionModal.tpl.html",
+					data: modalData,
+				}).then(function() {
+					acpaasportalversionbumperFactory.bumpVersion(modalData);
+				});
+			}
+
+			$scope.fetchItemsForType = fetchItemsForType;
+			$scope.searchItems = searchItems;
+
+			$scope.$on("bumpVersion", bumpVersion);
 
 			init();
 		},
--- public/app/factories/versionbumper.factory.js
+++ public/app/factories/versionbumper.factory.js
@@ -10,12 +10,29 @@ angular
 			$http,
 			configuration
 		) {
-			var api = configuration.serverPath + configuration.apiPrefix + configuration.apiLevel;
+			var api = configuration.serverPath + configuration.apiPrefix + configuration.apiLevel + "acpaasportalversionbumper";
 			var factory = {};
 
+			factory.fetchTypes = function() {
+				return $http.get(api + "/types")
+					.then(function(response) {
+						return response.data;
+					});
+			}
+
 			factory.fetchProducts = function() {
-				return $http.get(api + "acpaasportalversionbumper/products");
+				return $http.get(api + "/products")
+					.then(function(response) {
+						return response.data;
+					});
 			};
+
+			factory.fetchApis = function() {
+				return $http.get(api + "/apis")
+					.then(function(response) {
+						return response.data;
+					});
+			}
 
 			return factory;
 		},
