33f920eb876d087524a717b47b486145d30e50fe
Tom Opdebeeck <tom230691@gmail.com>
2017-08-10 17:57:06 +0200

    remove readone

--- app/controllers/products/index.js
+++ app/controllers/products/index.js
@@ -1,9 +1,7 @@
 var read = require("./read");
-var readOne = require("./readOne");
 var bumpVersion = require("./bumpVersion");
 
 module.exports = {
 	read: read,
-	readOne: readOne,
 	bumpVersion: bumpVersion,
 };
