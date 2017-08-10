87d60533a8e542af4623c0e821d9065f5309504a
Tom Opdebeeck <tom230691@gmail.com>
2017-08-10 17:57:56 +0200

    add types & apis routes

--- app/routes/versionbumper.js
+++ app/routes/versionbumper.js
@@ -2,14 +2,16 @@ require("rootpath")();
 
 var authHelper = require("../helpers/auth");
 var productsController = require("../controllers/products");
+var apisController = require("../controllers/apis");
+var contentTypesController = require("../controllers/contentTypes");
 
 // Get the configuration of the WCM
 var config = require("config")();
 // Building the baseUrl based on the configuration. Every API call needs to be located after the api/ route
-var baseUrl = "/" + config.api.prefix + config.api.version + "acpaasportal-versionbumper";
+var baseUrl = "/" + config.api.prefix + config.api.version + "acpaasportalversionbumper";
 
 module.exports = function(app) {
+	app.route(baseUrl + "/types").get(authHelper.prepareMember, contentTypesController.getTypes);
 	app.route(baseUrl + "/products").get(authHelper.prepareMember, productsController.read);
-	app.route(baseUrl + "/products/:product").get(authHelper.prepareMember, productsController.readOne);
-	app.route(baseUrl + "/products/:product/bump").put(authHelper.prepareMember, productsController.bumpVersion);
+	app.route(baseUrl + "/apis").get(authHelper.prepareMember, apisController.read);
 };
