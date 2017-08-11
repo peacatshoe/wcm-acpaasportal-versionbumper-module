993e2f765b0557169f8378ba17d8e1f9c7aaffda
Tom Opdebeeck <tom230691@gmail.com>
2017-08-12 00:05:34 +0200

    add helpers to index

--- app/controllers/helpers/index.js
+++ app/controllers/helpers/index.js
@@ -1,7 +1,11 @@
 var request = require("./request");
 var error = require("./error");
+var read = require("./read");
+var readOne = require("./readOne");
 
 module.exports = {
 	request: request,
-	error: error,
+    error: error,
+    read: read,
+    readOne: readOne,
 };
