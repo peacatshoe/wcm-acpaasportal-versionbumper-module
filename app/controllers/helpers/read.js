6e85be38068638677cacce4592d7d94d9ba0812b
Tom Opdebeeck <tom230691@gmail.com>
2017-08-12 00:05:28 +0200

    add readone helper + extend read helper

--- app/controllers/helpers/read.js
+++ app/controllers/helpers/read.js
@@ -5,11 +5,12 @@ var _ = require("lodash");
 
 var ContentModel = require("app/models/content");
 
-module.exports = function(contentType, query, fields) {
-	return ContentModel.find(_.extend({
+function find(contentType, q, f) {
+	var query = _.extend({
 		"meta.contentType": contentType,
 		"meta.deleted": false,
-	}, query), _.extend({
+	}, q);
+	var fields = f === true ? {} : _.extend({
 		_id: 0,
 		uuid: 1,
 		"meta.contentType": 1,
@@ -17,9 +18,23 @@ module.exports = function(contentType, query, fields) {
 		"meta.label": 1,
 		"meta.description": 1,
 		"meta.lastModified": 1,
-	}, fields))
-	.populate("meta.contentType")
-	.populate("meta.lastEditor")
-	.lean()
-	.exec();
+	}, f);
+
+	return ContentModel.find(query, fields);
+}
+
+module.exports = function(contentType, query, fields, populated) {
+	populated = populated !== undefined ? populated : true;
+
+	if (populated) {
+		return find(contentType, query, fields)
+			.populate("meta.contentType")
+			.populate("meta.lastEditor")
+			.lean()
+			.exec();
+	}
+
+	return find(contentType, query, fields)
+		.lean()
+		.exec();
 };
