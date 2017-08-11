6564abb4df7799cfb4878b2a2f74f0c07be46fd5
Tom Opdebeeck <tom230691@gmail.com>
2017-08-11 15:22:44 +0200

    cleanup

--- app/controllers/apis/read.js
+++ app/controllers/apis/read.js
@@ -5,37 +5,41 @@ var _ = require("lodash");
 var read = require("../helpers/read");
 var contentTypesHelper = require("../../helpers/contentTypes");
 
+function groupByServiceOrganisation(apis) {
+	return apis.reduce(function(acc, curr) {
+		var service = _.get(curr, "fields.service", "");
+		var organisation = _.get(curr, "fields.organisation", "");
+		var key = service + organisation !== "" ? service + organisation : "other";
+
+		acc[key] = acc[key] || [];
+
+		acc[key].push(curr);
+
+		return acc;
+	}, {});
+}
+
+function flattenGroupsToVersions(groups) {
+	return Object.keys(groups).map(function(group) {
+		return groups[group].reduce(function(acc, curr) {
+			if (!acc) {
+				return curr;
+			}
+
+			return _.get(acc, "fields.version", -1) > _.get(curr, "fields.version", -1) ? acc : curr;
+		}, null);
+	});
+}
+
 module.exports = function(req, res) {
-	read(contentTypesHelper().api, {
+	read(contentTypesHelper().api, null, {
 		"fields.organisation": 1,
 		"fields.service": 1,
 		"fields.version": 1,
 	})
-	.then(function(apis) {
-		var data = (apis || []).reduce(function(acc, curr) {
-			var service = _.get(curr, "fields.service", "");
-			var organisation = _.get(curr, "fields.organisation", "");
-			var key = service + organisation !== "" ? service + organisation : "other";
-
-			acc[key] = acc[key] || [];
-
-			acc[key].push(curr);
-
-			return acc;
-		}, {});
-
-		var results = Object.keys(data).map(function(group) {
-			return data[group].reduce(function(acc, curr) {
-				if (!acc) {
-					return curr;
-				}
-
-				return _.get(acc, "fields.version", -1) > _.get(curr, "fields.version", -1) ? acc : curr;
-			}, null);
+		.then(function(apis) {
+			res.status(200).json(flattenGroupsToVersions(groupByServiceOrganisation(apis)));
+		}, function(err) {
+			res.status(500).json(err);
 		});
-
-		res.status(200).json(results);
-	}, function(err) {
-		res.status(500).json(err);
-	});
 };
