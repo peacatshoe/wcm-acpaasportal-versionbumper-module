e52eb2d311133ff4224febe8cc84d854dd5949b3
Tom Opdebeeck <tom230691@gmail.com>
2017-08-12 02:25:36 +0200

    add some helpers

--- app/controllers/helpers/index.js
+++ app/controllers/helpers/index.js
@@ -2,10 +2,14 @@ var request = require("./request");
 var error = require("./error");
 var read = require("./read");
 var readOne = require("./readOne");
+var create = require("./create");
+var slug = require("./slug");
 
 module.exports = {
 	request: request,
-    error: error,
-    read: read,
-    readOne: readOne,
+	error: error,
+	read: read,
+	readOne: readOne,
+	create: create,
+	slug: slug,
 };
