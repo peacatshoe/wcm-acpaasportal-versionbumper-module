e2ca9f288b8a0f6c7ac7e88dce80174455c32e59
jeroenvalcke <valcke_jeroen@hotmail.com>
2017-11-10 18:45:27 +0100

    fix another require path

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 var contentTypes = require("./helpers/contentTypes");
 var versionBumperRoutes = require("./routes/versionbumper");
 var variablesHelper = require("./helpers/variables");
+var hooksController = require("./controllers/hooks");
 
 module.exports = function(app, hooks, info) {
 	variablesHelper.set(info);
 	// Setup hooks
-	require("../controllers/hooks")(hooks);
+	hooksController(hooks);
 	// Update contentTypes
 	contentTypes.reload();
 	// Setup routes
